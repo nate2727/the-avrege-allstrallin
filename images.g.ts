d652be5fba56dce56763080fdaad6049426e66e1
nate2727 <135049746+nate2727@users.noreply.github.com>
2023-05-30 12:31:40 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,27 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "the song":return hex`0078000408020108001c000e050046006603320000040a002d00000064001400010a0002000002450000000400012a04000800012a08000c0001270c00100001271000140001221400180002272918001c0001292000240002222728002c0002252a2c003000011e300034000124`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
